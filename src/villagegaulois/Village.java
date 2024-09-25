d9dfa2a70adfb4f195ee47116ecbec09daac0863
Anteneh bewnet Amdeberhan <mdn4829a@ensfsi.onmicrosoft.com>
2024-09-25 12:00:00 +0200

    TP1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -56,4 +56,73 @@ public class Village {
 		}
 		return chaine.toString();
 	}
+	
+	private static class Marche{
+		private Etal[] etals;
+		private Marche(int nbEtal) {
+			etals = new Etal[nbEtal];
+			for(int i=0; i<nbEtal;i++) {
+				etals[i] = new Etal();
+			}
+		}
+		
+		void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur,produit,nbProduit);
+		}
+		
+		int trouverEtalLibre() {
+			int i  = 0;
+			while(i<etals.length && etals[i].isEtalOccupe()) {
+				i++;
+			}
+			if (i == etals.length) {
+				return -1;
+			}
+			return i;
+		}
+		
+		 private Etal[] trouverEtals(String produit) {
+			 Etal[] etalProduit;
+			 int j = 0;
+			 for(int i = 0; i< etals.length; i++) {
+				 if (etals[i].contientProduit(produit)) {
+					 j++;
+				 }
+			 }
+			 etalProduit = new Etal[j];
+			 int k = 0;
+			 for(int i =0; i<etals.length;i++) {
+				 if (etals[i].contientProduit(produit)) {
+					 etalProduit[k] = etals[i];
+					 k++;
+				 }
+			 }
+			 return etalProduit;
+		 }
+		 
+		 private Etal trouverVendeur(Gaulois gaulois) {
+			 for(int i = 0;  i< etals.length; i++) {
+				 if (etals[i].getVendeur().equals(gaulois)){
+					 return etals[i];
+				 }		
+			 }
+			 return null;
+		 }
+		 
+		 private String afficherMarche() {
+			 int nbEtalVide = 0;
+			 StringBuilder chaine = new StringBuilder();
+			 for(int i = 0;i<etals.length;i++) {
+				 if(!etals[i].isEtalOccupe()) {
+					 nbEtalVide++;
+				 }else {
+					 chaine.append(etals[i].afficherEtal());
+				 }
+			 }
+			 if (nbEtalVide > 0) {
+				 return  "Il reste " + nbEtalVide + " étals non utilisés dans le marché.\n."; 
+			 }
+		 }
+	}
+	
 }
